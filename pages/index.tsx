a3c617ac909d01394ca8bc329ebba015fb100d78
Tarun-kumar-singh <tarunks2512@gmail.com>
2023-11-14 01:07:16 +0530

    add items to todo

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,33 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useForm } from 'react-hook-form'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TaskForm from '../components/TakForm'
 
 const Home: NextPage = () => {
  
-  const [displayForm, setDisplayForm] = useState(false)
+  const [itemsTodo, setItemsTodo] = useState<Array<any>>([
+    { id: '1', name: 'Name 1', description: 'This is a description 1 of name 1' },
+    { id: '2', name: 'Name 2', description: 'This is a description 2 of name 2' },
+    { id: '3', name: 'Name 3', description: 'This is a description 3 of name 3' },
+  ]);
+  const [displayForm, setDisplayForm] = useState<boolean>(false)
   
+
+  useEffect(() =>{
+console.log(itemsTodo)
+  }, [itemsTodo])
+
   const onAddTask = (data: any) =>{
     console.log(data)
+    const newTask = { id: (itemsTodo.length + 1).toString(), ...data }
+    setItemsTodo([ newTask, ...itemsTodo ])
   }
 
   const onCacelForm = () =>{
     setDisplayForm(false)
   }
 
-  const onClickAddTask = () =>{
-    setDisplayForm(true)
-  }
 
   return (
     <div>
