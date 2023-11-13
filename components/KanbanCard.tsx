5fb2521c02f0394771d62850975350c9ebf40434
Tarun-kumar-singh <tarunks2512@gmail.com>
2023-11-14 01:42:57 +0530

    cards height and width changes

--- components/KanbanCard.tsx
+++ components/KanbanCard.tsx
@@ -4,7 +4,7 @@
 const KanbanCard = ({ name, description }: any) =>{
 
     return(
-        <div className="w-56 h-44 rounded overflow-hidden shadow-lg bg-red-50">
+        <div className="w-45 h-40 rounded overflow-hidden shadow-lg bg-red-50">
       
         <div className="px-2 py-2 text-center">
             <div className="font-bold text-l mb-2 text-center">
