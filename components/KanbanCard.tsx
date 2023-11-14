f23666f43044611de209f63e711f2652225f7f00
Tarun-kumar-singh <tarunks2512@gmail.com>
2023-11-14 13:33:50 +0530

    add delete icon

--- components/KanbanCard.tsx
+++ components/KanbanCard.tsx
@@ -1,21 +1,29 @@
+import { MdDelete } from "react-icons/md";
 
 
-
-const KanbanCard = ({ name, description }: any) =>{
+const KanbanCard = ({ item, index, onDelete }: any) =>{
 
     return(
-        <div className="w-45 h-36 rounded overflow-hidden shadow-lg bg-red-50">
+        <div className="w-45 h-36 rounded overflow-hidden shadow-lg bg-red-50 relative">
       
-        <div className="px-2 py-2 text-center">
-            <div className="font-bold text-l mb-2 text-center">
-              {name}
+            <div className="px-2 py-2 text-center">
+                <div className="font-bold text-l mb-2 text-center">
+                {item.name}
+                </div>
+        
+                <p className="text-gray-700 text-base">
+                {item.description}
+                </p>
             </div>
-    
-            <p className="text-gray-700 text-base">
-              {description}
-            </p>
+
+            <div className="absolute bottom-0 right-0 mb-1 mr-2">
+                <span onClick={() =>onDelete(item, index)} className="inline-block bg-gray-200 rounded-full px-2 py-1 text-gray-700">
+                <MdDelete />
+
+                </span>
+            </div>
+ 
         </div>
-    </div>
     )
 }
 
